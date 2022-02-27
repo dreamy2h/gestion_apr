40100334bd8e05230c2f72f6bcd86ce613747766
zamyr pedreros <zamyrpedreros@gmail.com>
2022-02-27 13:37:11 -0300

    Se crea lecturas por sector

--- app/Controllers/Ctrl_menu.php
+++ app/Controllers/Ctrl_menu.php
@@ -335,6 +335,11 @@
 			echo view('Informes/informe_deudores');	
 		}
 
+		public function lectura_sector() {
+			$this->validar_sesion();
+			echo view('Consumo/lectura_sector');
+		}
+
 		public function validar_sesion() {
 			if (!$this->sesión->has("id_usuario_ses")) {
 				echo "La sesión expiró, actualice el sitio web con F5";
